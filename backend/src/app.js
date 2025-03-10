111cd4bdf8999af6026c20863712e9118993e0ad
Nehal Jain <niihaljain2005@gmail.com>
2025-03-10 21:48:33 +0530

    setup server for application

---
+++ backend/src/app.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";    
+
+const app = express();
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}));
+    
+app.use(express.json({limit: "1mb"}));
+app.use(express.urlencoded({extended: true, limit: "1mb"}));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+export {app}
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,15 @@
 import dotenv from "dotenv";
 import connectDB from "./db/connection.js";
+import { app } from "./app.js";
 dotenv.config();
 
-// connecting to MongoDB
-connectDB();
-
-
+// connecting to MongoDB and setting up Server
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running at port: ${process.env.PORT}`);
+        })
+    })
+    .catch((error) => {
+        console.log("MongoDB connection failed! ", error);
+    })
